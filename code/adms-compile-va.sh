935c02390cf0c256fb64952938c82004b1a0dd09
dwarning <dwarning>
2023-08-26 18:00:27 +0200

    add mosvar

--- code/adms-compile-va.sh
+++ code/adms-compile-va.sh
@@ -10,38 +10,38 @@ mkdir -p admslibs
 cd ./bsim4/vacode
 buildxyceplugin bsim4.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 cd ./bsim6/vacode
 buildxyceplugin BSIM6.1.1.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 cd ./bsimbulk/vacode
 #buildxyceplugin bsimbulk106.va ../../admslibs
 buildxyceplugin bsimbulk107.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 cd ./bsimcmg/vacode110
 buildxyceplugin bsimcmg.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 #buildxyceplugin bsimimg.va ../../admslibs
 cd ./bsimsoi/vacode
 buildxyceplugin bsimsoi.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 cd ./diode_cmc/vacode
 buildxyceplugin diode_cmc.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 cd ./ekv/vacode
 buildxyceplugin ekv26.va ../../admslibs
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 rm *.la *.log
 cd ../..
 #buildxyceplugin ../EPFL-HEMT/vacode/epfl_hemt.va .
@@ -51,7 +51,7 @@ cd ../..
 cd ./hicum2/vacode
 buildxyceplugin hicumL2.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 #buildxyceplugin ../hisim2/vacode/hisim2.va .
 #buildxyceplugin ../hisimhv/vacode242/hisimhv.va .
@@ -61,22 +61,26 @@ cd ../..
 cd ./IGBT/vacode
 buildxyceplugin nigbt.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 #buildxyceplugin ../L-UTSOI/vacode/L_UTSOI_102.va .
 cd ./mextram/504p12p1_vacode
 buildxyceplugin bjt504.va ../../admslibs
 buildxyceplugin bjt504t.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 cd ./mextram/505p2p0_vacode
 buildxyceplugin bjt505.va ../../admslibs
 buildxyceplugin bjt505t.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
-#buildxyceplugin ../MOSVAR/vacode/mosvar.va .
+cd ./MOSVAR/vacode
+buildxyceplugin mosvar.va ../../admslibs
+rm *.la *.log
+rm -rfd .libs/
+cd ../..
 #buildxyceplugin ../mvsg/vacode/mvsg_cmc.va .
 #buildxyceplugin ../psp102/vacode/psp102.va .
 #buildxyceplugin ../psp103/vacode/psp103.va .
@@ -85,12 +89,12 @@ cd ../..
 cd ./r3_cmc/vacode
 buildxyceplugin r3_cmc.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 cd ./vbic/vacode
 buildxyceplugin vbic_1p3.va ../../admslibs
 rm *.la *.log
-rmdir --ignore-fail-on-non-empty .libs
+rm -rfd .libs/
 cd ../..
 
 echo done
