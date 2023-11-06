d7040c90d185347a853b473e01c39ae4e7140318
dwarning <dwarning>
2023-11-06 14:48:32 +0100

    update hicum0 model to version 2.1.0

--- code/openvaf-compile-va.sh
+++ code/openvaf-compile-va.sh
@@ -22,7 +22,7 @@ openvaf -D__NGSPICE__ -o ekv26.osdi ../ekv/vacode/ekv26.va
 openvaf -D__NGSPICE__ -o epfl_hemt.osdi ../EPFL-HEMT/vacode/epfl_hemt.va
 openvaf -D__NGSPICE__ -o fbh_hbt-2_1.osdi ../fbh_hbt/vacode/fbh_hbt-2_1.va
 openvaf -D__NGSPICE__ -o fbh_hbt-2_3.osdi ../fbh_hbt/vacode/fbh_hbt-2_3.va
-openvaf -D__NGSPICE__ -o hicumL0_v2p0p0.osdi ../hicum0/vacode/hicumL0_v2p0p0.va
+openvaf -D__NGSPICE__ -o hicumL0_v2p1p0.osdi ../hicum0/vacode/hicumL0_v2p1p0.va
 openvaf -D__NGSPICE__ -o hicumL2V2p4p0.osdi ../hicum2/vacode/hicumL2.va
 openvaf -D__NGSPICE__ -o hicumL2.osdi ../hicum2/vacode/hicumL2V3p0p0.va
 openvaf -D__NGSPICE__ -o hisim2.osdi ../hisim2/vacode/hisim2.va
