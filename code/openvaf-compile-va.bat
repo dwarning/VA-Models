7f71492d258c0925ed795485a5f7340b5104d952
dwarning <dwarning>
2025-07-13 18:39:06 +0200

    hicum2 convergence help

--- code/openvaf-compile-va.bat
+++ code/openvaf-compile-va.bat
@@ -26,7 +26,7 @@ openvaf.exe -D__NGSPICE__ -o epfl_hemt.osdi ..\code\EPFL-HEMT\vacode\epfl_hemt.v
 openvaf.exe -D__NGSPICE__ -o fbh_hbt-2_1.osdi ..\code\fbh_hbt\vacode\fbh_hbt-2_1.va
 openvaf.exe -D__NGSPICE__ -o fbh_hbt-2_3.osdi ..\code\fbh_hbt\vacode\fbh_hbt-2_3.va
 openvaf.exe -D__NGSPICE__ -o hicumL0_v2p1p0.osdi ..\code\hicum0\vacode\hicumL0_v2p1p0.va
-openvaf.exe -D__NGSPICE__ -o hicumL2V2p4p0.osdi ..\code\hicum2\vacode\hicumL2.va
+openvaf.exe -D__NGSPICE__ -o hicumL2V2p4p0.osdi ..\code\hicum2\vacode\hicumL2V2p4p0.va
 openvaf.exe -D__NGSPICE__ -o hicumL2.osdi ..\code\hicum2\vacode\hicumL2_v310.va
 openvaf.exe -D__NGSPICE__ -o hisim2.osdi ..\code\hisim2\vacode\hisim2.va
 openvaf.exe -D__NGSPICE__ -o hisimhv.osdi ..\code\hisimhv\vacode\hisimhv.va
